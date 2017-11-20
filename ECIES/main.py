66e0de9339fdb43736c3f6d5b140c628d3bcf256
tpnguye5 <31353832+tpnguye5@users.noreply.github.com>
2017-11-19 23:54:16 -0800

    Changed main.py

--- ECIES/main.py
+++ ECIES/main.py
@@ -1,6 +1,11 @@
+"""
+ECIES PROJECT-Theresa Nguyen
+MODES: Encryption and Decryption
+METHOD:
+Asymmetric and symmetric encryption and decryption.
+"""
 
 import sys, getopt, os, hashlib, Chacha_poly
-
 
 from os import urandom
 from ecc import string_to_int, int_to_string
@@ -40,36 +45,38 @@ def b58encode(v):
 
   return (__b58chars[0]*nPad) + result
 
-def key_dev(z):
-    # HMAC, in this case, you hashlib sha 256
+
+def KDF(z):
     m = hashlib.sha256(int_to_string(z))
+
     return m
 
 class KeyPair(object):
+
 
     @classmethod
     def new_keypair(self):
         curve = SECP_256k1()
         G = curve.generator()
+
         self.private = string_to_int(os.urandom(curve.coord_size))
         self.public = self.private * G
-        # self.private = urandom(curve.coord_size)
-        # self.public = curve.generator()
 
-        priv_b58 = b58encode(int_to_string(u_private))
-        pub_b58_x = b58encode(int_to_string(U_public.x))
-        pub_b58_y = b58encode(int_to_string(U_public.y))
+        priv_b58 = b58encode(int_to_string(self.private))
+        pub_b58_x = b58encode(int_to_string(self.public.x))
+        pub_b58_y = b58encode(int_to_string(self.public.y))
 
-        z = self.private * self.public
+        key_agreement = self.private * self.public
 
-        V = key_dev(z)  #MAC
+        key = KDF(key_agreement)  #MAC
 
-        return V
+        return key
 
-    def save_to_file(vector, file):
+    def save_to_file(sharedKey, file):
+        #only thing saved to the file is the agreed shared key
         file = outfile
         ofile = open(file, "w")
-        ofile.write(vector)
+        ofile.write(sharedKey)
 
     def get_from_file(infile):
         file = infile
@@ -77,26 +84,51 @@ class KeyPair(object):
             return line
 
 def ecies_encryption(target_pub_key, file):
+    outfile ="saved_shared_key.txt"
+
     #use chacha_poly_aead
     empheral_key_pair = KeyPair()
-    secret_key = key_dev(int_to_string((empheral_key_pair.private*target_pub_key).x))
+    secret_key = KDF(int_to_string((empheral_key_pair.private*target_pub_key).x))
 
+    KeyPair.save_to_file(secret_key,outfile)
+
+    ad=""
     iv= urandom(12)
     constant = ""
 
     #throw iv, constant, into cha cha
     #aad, what is it...
-    alg = ChaCha_Poly_AEAD(secret_key ,constant)
-    #aad, iv, plain text
-    message = alg.encrypt("", iv, file)
-    #reult is the concatenation of the symmetric encryption key
-    tag = int_to_string(secret_key) + message
+    alg = ChaCha_Poly_AEAD(secret_key, constant)
 
-    return tag
+    # aad, iv, plain text
+    message = alg.encrypt(ad, iv, file)  #returns the iv, cipher text, and tag
+
+    return int_to_string(KeyPair.public) + message
 
 #Cipher text includes: U + IV + ciphertext + tag (look at presentation)
-def encis_decryption(ephemeral_pub_key, tag, cipher_text):
+def encis_decryption(sender_public_key, tag, cipher_text, infile):
     #with the public key and his private key, v, Bob will multiply both elements in order to produce the shared secret value
+    empheral_key_pair = KeyPair()
+    shared_secret_value = sender_public_key * empheral_key_pair.private
+
+    m = KDF(shared_secret_value)
+
+    #check the save key from the file
+    key_from_infile = KeyPair.get_from_file(infile)
+
+    constant =""
+
+    ####
+    #I don't know how to check the tags
+    ####
+    if key_from_infile == m:
+        ad = ""
+        alg = ChaCha_Poly_AEAD(shared_secret_value, constant)
+        message = alg.decrypt(ad, cipher_text)
+        return message
+
+    else:
+        return "REJECTION: FAILURE IN MAC VERIFICATION"
 
 
 # TODO: if there isn't a file...
@@ -120,10 +152,13 @@ def main(argv):
         elif opt in ("-o", "--ofile"):
             outputfile = arg
         elif opt in ("-e", "--encrypt"):
-            ecies_encrypt(public_key, data) #TODO: change this
+            target_public_key = input("target public key required:")
+            ecies_encryption(target_public_key, inputfile)
 
         elif opt in ("-d", "--decrypt"):
-            encis_decrypt(secret_key, data)  #TODO: change this
+            # sender_public_key, tag, cipher_text, infile
+            sender_public_key = input("sender's public key required:")
+            encis_decryption(sender_public_key, tag, inputfile, infile)
         else:
             print()
 
